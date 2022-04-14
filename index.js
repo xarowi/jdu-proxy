89b698fc8093960f6207bb51582bf55a84465201
xarowi <serebryakov_2007@internet.ru>
2022-04-14 13:23:40 +0500

    Application configuration endpoint

--- index.js
+++ index.js
@@ -6,16 +6,28 @@ require("dotenv").config();
 
 const express = require("express");
 const crypto = require("crypto");
+const fs = require("fs");
+const res = require("express/lib/response");
 
 const app = express();
-const port = process.env.PORT || "8080";
+const port = process.env.PORT || "80";
 
 // JSON body limited to 2 kibibyte
 app.use(express.json({ limit: 2048 }));
 
-// Test endpoint
-app.get("/", function (request, response) {
-  response.send("");
+/**
+ * Application configuration endpoint
+ */
+app.get("/v1/applications/:applicationId/configuration", async function (request, response) {
+  const { applicationId } = request.params;
+
+  let configurationCheck = fs.existsSync(`./configurations/${applicationId}.json`);
+  if (configurationCheck) {
+    const configuration = require(`./configurations/${applicationId}.json`);
+    return response.json(configuration);
+  }
+
+  response.sendStatus(404);
 });
 
 app.listen(port, function () {
