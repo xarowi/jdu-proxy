2a11ef27e1ef42d93a88b9753883f910f806660a
xarowi <serebryakov_2007@internet.ru>
2022-04-14 13:35:30 +0500

    Entities endpoint

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ require("dotenv").config();
 const express = require("express");
 const crypto = require("crypto");
 const fs = require("fs");
-const res = require("express/lib/response");
 
 const app = express();
+
 const port = process.env.PORT || "80";
+const baseUrl = `http://localhost:${port}`;
 
 // JSON body limited to 2 kibibyte
 app.use(express.json({ limit: 2048 }));
@@ -18,13 +19,59 @@ app.use(express.json({ limit: 2048 }));
 /**
  * Application configuration endpoint
  */
-app.get("/v1/applications/:applicationId/configuration", async function (request, response) {
-  const { applicationId } = request.params;
+app.get(
+  "/v1/applications/:applicationId/configuration",
+  async function (request, response) {
+    const { applicationId } = request.params;
 
-  let configurationCheck = fs.existsSync(`./configurations/${applicationId}.json`);
-  if (configurationCheck) {
-    const configuration = require(`./configurations/${applicationId}.json`);
-    return response.json(configuration);
+    let configurationCheck = fs.existsSync(
+      `./configurations/${applicationId}.json`
+    );
+    if (configurationCheck) {
+      const configuration = require(`./configurations/${applicationId}.json`);
+
+      configuration.gatewayResources.map(function (gatewayResource) {
+        const { name, url } = gatewayResource;
+
+        if (name === "spaces/entities") {
+          let newUrl = url.replace(
+            "https://{env}public-ubiservices.ubi.com",
+            baseUrl
+          );
+          gatewayResource.url = newUrl;
+        }
+
+        return gatewayResource;
+      });
+
+      return response.json({ configuration });
+    }
+
+    response.sendStatus(404);
+  }
+);
+
+/**
+ * Entities endpoint
+ */
+app.get("/v2/spaces/:spaceId/entities", async function (request, response) {
+  const { spaceId } = request.params;
+
+  let entitiesCheck = fs.existsSync(`./entities/${spaceId}.json`);
+  if (entitiesCheck) {
+    const entities = require(`./entities/${spaceId}.json`);
+
+    entities.map(function (entity) {
+      let { obj } = entity;
+
+      if (!obj.url) {
+        entity.obj.url = baseUrl;
+      }
+
+      return entity;
+    });
+
+    return response.json({ entities });
   }
 
   response.sendStatus(404);
